f2b50ff489d59a915d5b503edd96b2709bcd85bb
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-11 17:46:17 +0530

    (feat): add long double and float precision

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -19,7 +19,7 @@
 /** Constant global double */ const double PI = 3.141;
 
 /**  
- * This is our main function, and is run everytime we run the program
+ * This is our main function, and is run everytime we run the program.
  * @param argc number of args passed in main
  * @param argv array of pointers to strings
  *  
@@ -47,5 +47,24 @@ int main(int argc, char **argv)
     std::cout << "Min : long > " << std::numeric_limits<long>::min() << std::endl;
     std::cout << "Max : long > " << std::numeric_limits<long>::max() << std::endl;
 
+    std::cout << "Min : float > " << std::numeric_limits<float>::min() << std::endl;
+    std::cout << "Max : float > " << std::numeric_limits<float>::max() << std::endl;
+
+    std::cout << "Min : double > " << std::numeric_limits<double>::min() << std::endl;
+    std::cout << "Max : double > " << std::numeric_limits<double>::max() << std::endl;
+
+    std::cout << "Min : long double > " << std::numeric_limits<long double>::min() << std::endl;
+    std::cout << "Max : long double > " << std::numeric_limits<long double>::max() << std::endl;
+
+    /**
+     * @see floats are accurate upto 6 decimal places
+    */
+    float fx = 1.1111111;
+    float fy = 1.1111111;
+
+    /** @param printf provides printing with formatting */
+    printf("Sum = %.7f\n", (fx + fy)); // Sum = 2.2222221 << lost precision
+
+    /** @return 0 {integer}, standard output for main signals successful run */
     return 0;
 }
