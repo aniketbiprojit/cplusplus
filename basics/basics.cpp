d3c80f311443035b0c0c3003de8aeab67a5d87de
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-11 17:36:12 +0530

    (feat): add min max

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -15,8 +15,8 @@
  * ` using namespace std; `
  */
 
-/* Global */ int globalVar = 0;
-/* Constant */ const double PI = 3.141;
+/** Global int */ int globalVar = 0;
+/** Constant global double */ const double PI = 3.141;
 
 /**  
  * This is our main function, and is run everytime we run the program
@@ -28,6 +28,24 @@
 int main(int argc, char **argv)
 {
     std::cout << "Welcome to the basics file." << std::endl;
+
+    /** @private Boolean */ bool trueValue = true;
+    /** @private Charachter */ char letter = 'A';
+
+    /**
+     * @see now we print the min max of the most commanly used datatypes
+    */
+    std::cout << "Min : (unsigned) short int > " << std::numeric_limits<unsigned short int>::min() << std::endl;
+    std::cout << "Max : (unsigned) short int > " << std::numeric_limits<unsigned short int>::max() << std::endl;
+
+    std::cout << "Min : short int > " << std::numeric_limits<short int>::min() << std::endl;
+    std::cout << "Max : short int > " << std::numeric_limits<short int>::max() << std::endl;
+
+    std::cout << "Min : int > " << std::numeric_limits<int>::min() << std::endl;
+    std::cout << "Max : int > " << std::numeric_limits<int>::max() << std::endl;
+
+    std::cout << "Min : long > " << std::numeric_limits<long>::min() << std::endl;
+    std::cout << "Max : long > " << std::numeric_limits<long>::max() << std::endl;
 
     return 0;
 }
