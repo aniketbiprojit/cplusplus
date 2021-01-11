65308e1a551c3f0d97cd927543dbcc10523f382f
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-11 16:58:36 +0530

    (feat): add optional std declaration

--- basics/basics.cpp
+++ basics/basics.cpp
@@ -8,7 +8,16 @@
 #include <ctime>
 #include <cmath>
 
+/** I will be avoiding declaring namespaces
+ * 
+ * You can add this code below and remove all std:: declarations
+ * ` using namespace std; `
+ * 
+ */
+
 int main(int argc, char **argv)
 {
+    std::cout << "Welcome to the basics file." << std::endl;
+
     return 0;
 }
